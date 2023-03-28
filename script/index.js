82fb0993e40077bbf89d1397de271c86a690a21b
Mire27 <mirepatel.19bcp080@gmail.com>
2023-03-28 19:46:26 +0530

    modification1.1.0

--- script/index.js
+++ script/index.js
@@ -1,32 +1,52 @@
 let movies = [
     {
         name: "falcon and the winter soldier",
-        des:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+        des1:
+            "2021\u00A0\u00A0•\u00A0\u00A01 Season\u00A0\u00A0•\u00A0\u00A04 Languages\u00A0\u00A0•\u00A0\u00A0U/A 13+",
+        des2:
+            "Following the events of “Avengers: Endgame,” Sam Wilson and Bucky Barnes team up in a global adventure that tests their abilities and their patience.",
+        des3:
+            "Superhero\u00A0\u00A0|\u00A0\u00A0Action\u00A0\u00A0|\u00A0\u00A0Science Fiction\u00A0\u00A0|\u00A0\u00A0Fantasy",
         image: "images/slider 2.png"
     },
     {
         name: "loki",
-        des:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+        des1:
+            "2021\u00A0\u00A0•\u00A0\u00A01 Season\u00A0\u00A0•\u00A0\u00A02 Languages\u00A0\u00A0•\u00A0\u00A0U/A 13+",
+        des2:
+            "The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of “Avengers: Endgame.”",
+        des3:
+            "Superhero\u00A0\u00A0|\u00A0\u00A0Science Fiction\u00A0\u00A0|\u00A0\u00A0Action\u00A0\u00A0|\u00A0\u00A0Fantasy\u00A0\u00A0|\u00A0\u00A0Anti-hero",
         image: "images/slider 1.png"
     },
     {
         name: "wanda vision",
-        des:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+        des1:
+            "2021\u00A0\u00A0•\u00A0\u00A01 Season\u00A0\u00A0•\u00A0\u00A0English\u00A0\u00A0•\u00A0\u00A0U/A 13+",
+        des2:
+            "Wanda Maximoff and Vision—two super-powered beings living idealized suburban lives—begin to suspect that everything is not as it seems.",
+        des3:
+            "Superhero\u00A0\u00A0|\u00A0\u00A0Science Fiction\u00A0\u00A0|\u00A0\u00A0Action\u00A0\u00A0|\u00A0\u00A0Fantasy",
         image: "images/slider 3.png"
     },
     {
         name: "raya and the last dragon",
-        des:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+        des1:
+            "2021\u00A0\u00A0•\u00A0\u00A01h 47m\u00A0\u00A0•\u00A0\u00A04 Languages\u00A0\u00A0•\u00A0\u00A0U/A 7+",
+        des2:
+            "Raya, a fallen princess, must track down the legendary last dragon to stop the evil forces that have returned and threaten her world.",
+        des3:
+            "Animation\u00A0\u00A0|\u00A0\u00A0Fantasy\u00A0\u00A0|\u00A0\u00A0Adventure\u00A0\u00A0|\u00A0\u00A0Kids",
         image: "images/slider 4.png"
     },
     {
         name: "luca",
-        des:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+        des1:
+            "2021\u00A0\u00A0•\u00A0\u00A01h 35m\u00A0\u00A0•\u00A0\u00A04 Languages\u00A0\u00A0•\u00A0\u00A0U/A 7+",
+        des2:
+            "The movie is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides.",
+        des3:
+            "Animation\u00A0\u00A0|\u00A0\u00A0Fantasy\u00A0\u00A0|\u00A0\u00A0Adventure\u00A0\u00A0|\u00A0\u00A0Kids",
         image: "images/slider 5.png"
     }
 ];
@@ -46,14 +66,29 @@ const createSlide = () => {
     var imgElement = document.createElement("img");
     let content = document.createElement("div");
     let h1 = document.createElement("h1");
-    let p = document.createElement("p");
+    let p1 = document.createElement("p");
+    let p2 = document.createElement("p");
+    let p3 = document.createElement("p");
+    let wnaddbtns = document.createElement("div");
+    let wnbtn = document.createElement("div");
+    let addbtn = document.createElement("div");
 
     // attaching all elements
     imgElement.appendChild(document.createTextNode(""));
     h1.appendChild(document.createTextNode(movies[slideIndex].name));
-    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    p1.appendChild(document.createTextNode(movies[slideIndex].des1));
+    p2.appendChild(document.createTextNode(movies[slideIndex].des2));
+    p3.appendChild(document.createTextNode(movies[slideIndex].des3));
+    wnaddbtns.appendChild(document.createTextNode(""));
+    wnbtn.appendChild(document.createTextNode("Watch Now"));
+    addbtn.appendChild(document.createTextNode(""));
     content.appendChild(h1);
-    content.appendChild(p);
+    content.appendChild(p1);
+    content.appendChild(p2);
+    content.appendChild(p3);
+    wnaddbtns.appendChild(wnbtn);
+    wnaddbtns.appendChild(addbtn);
+    content.appendChild(wnaddbtns);
     slide.appendChild(content);
     slide.appendChild(imgElement);
     carousel.appendChild(slide);
@@ -66,7 +101,12 @@ const createSlide = () => {
     slide.className = "slider";
     content.className = "slide-content";
     h1.className = "movie-title";
-    p.className = "movie-des";
+    p1.className = "movie-des1";
+    p2.className = "movie-des2";
+    p3.className = "movie-des3";
+    wnaddbtns.className = "wnadd-btns";
+    wnbtn.className = "wn-btn";
+    addbtn.className = "add-btn";
 
     sliders.push(slide);
 
@@ -82,7 +122,7 @@ for (let i = 0; i < 3; i++) {
 
 setInterval(() => {
     createSlide();
-}, 3000);
+}, 4000);
 
 //Video Cards
 
